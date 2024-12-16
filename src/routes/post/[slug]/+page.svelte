da61c657d344f7eb7dab6bcef6d9cf8d2b0c80f5
Will Greenberg <ifnspifn@gmail.com>
2024-12-16 00:32:55 -0800

    rm unused comment component

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import MainBox from "$lib/components/MainBox.svelte";
     import TitleBox from "$lib/components/TitleBox.svelte";
-    import CommentSection from "$lib/components/CommentSection.svelte";
     import type { PageData } from "./$types";
     let { data }: { data: PageData } = $props();
 
